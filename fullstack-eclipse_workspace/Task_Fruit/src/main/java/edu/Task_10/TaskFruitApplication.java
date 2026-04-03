4529e2d6bfd9c1461727b1f51bf981e942fdfa41
Indrani1725 <tirumaniindrani@gmail.com>
2026-04-03 20:23:56 +0530

    task files added

---
+++ fullstack-eclipse_workspace/Task_Fruit/src/main/java/edu/Task_10/TaskFruitApplication.java
@@ -0,0 +1,13 @@
+package edu.Task_10;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskFruitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskFruitApplication.class, args);
+	}
+
+}
